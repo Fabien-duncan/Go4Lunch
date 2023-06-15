c7b2f6397bf38045a8cef6c03e32d3acab22993c
Fabien Duncan <fabien.duncan@gmail.com>
2023-06-15 17:02:42 +0200

    side menu added logout functionality, however there are still some bugs to fix.

--- app/src/main/java/com/example/go4lunch/views/MapsActivity.java
+++ app/src/main/java/com/example/go4lunch/views/MapsActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.go4lunch.MainActivity;
 import com.example.go4lunch.R;
@@ -26,14 +28,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private TextView name;
     private BottomNavigationView menu;
     private MapViewFragment mMapViewFragment;
     private ListViewFragment mListViewFragment;
     private WorkmatesFragment mWorkmatesFragment;
     private MainActivityViewModel mMainActivityViewModel;
-    private FloatingActionButton signOutbtn;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     private Toolbar mToolbar;
@@ -43,13 +44,15 @@ public class MapsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        name = findViewById(R.id.outputName_tv);
+
         menu = findViewById(R.id.bottomNavigationView);
-        signOutbtn = findViewById(R.id.signOut_fb);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mNavigationView = findViewById(R.id.nav_view);
         mToolbar = findViewById(R.id.toolbar);
+        View sideBarView = mNavigationView.getHeaderView(0);
+        name = sideBarView.findViewById(R.id.side_menu_display_name);
+
 
         mMapViewFragment = new MapViewFragment();
         mListViewFragment = new ListViewFragment();
@@ -68,17 +71,13 @@ public class MapsActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        mNavigationView.bringToFront();
+        mNavigationView.setNavigationItemSelectedListener(this);
         mMainActivityViewModel.getIsUserSignedIn().observe(this, new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
                 if(!aBoolean)showMainActivity();
-            }
-        });
-        signOutbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMainActivityViewModel.signOut();
-                System.out.println("singOut");
             }
         });
 
@@ -110,5 +109,32 @@ public class MapsActivity extends AppCompatActivity {
         startActivity(intent);
 
         finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.side_bar_lunch:
+                Toast.makeText(this, "view lunch!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.side_bar_settings:
+                Toast.makeText(this, "view Settings!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.side_bar_logout:
+                mMainActivityViewModel.signOut();
+                System.out.println("singOut");
+                break;
+        }
+        return true;
     }
 }
