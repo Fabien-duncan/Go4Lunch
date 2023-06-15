cf336d72d6b4c30c87a4434f29388c11a2d9feec
Fabien Duncan <fabien.duncan@gmail.com>
2023-06-15 16:26:33 +0200

    side menu pops up but functionality needs to be added.

--- app/src/main/java/com/example/go4lunch/views/MapsActivity.java
+++ app/src/main/java/com/example/go4lunch/views/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.go4lunch.views;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -21,6 +24,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MapsActivity extends AppCompatActivity {
     private TextView name;
@@ -30,6 +34,9 @@ public class MapsActivity extends AppCompatActivity {
     private WorkmatesFragment mWorkmatesFragment;
     private MainActivityViewModel mMainActivityViewModel;
     private FloatingActionButton signOutbtn;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
 
 
     @Override
@@ -40,9 +47,15 @@ public class MapsActivity extends AppCompatActivity {
         menu = findViewById(R.id.bottomNavigationView);
         signOutbtn = findViewById(R.id.signOut_fb);
 
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+        mToolbar = findViewById(R.id.toolbar);
+
         mMapViewFragment = new MapViewFragment();
         mListViewFragment = new ListViewFragment();
         mWorkmatesFragment = new WorkmatesFragment();
+
+        setSupportActionBar(mToolbar);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, mMapViewFragment).commit();
 
@@ -52,6 +65,9 @@ public class MapsActivity extends AppCompatActivity {
 
         name.setText(userName);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         mMainActivityViewModel.getIsUserSignedIn().observe(this, new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
